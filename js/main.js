596ccf33c586016cf22f7ee853347c6888695e67
bellahong95 <bellahong95@gmail.com>
2021-12-15 19:22:14 +0900

    button 클릭시 필터링 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 
+loadItems()
+.then(items => {
+    displayItems(items);
+    setEventListener(items);
+})
+
 function loadItems(){
     return fetch('../data/data.json')
     .then(response => response.json())
     .then(json => json.items)
 }
-
-loadItems()
-.then(items => {
-    displayItems(items);
-
-})
-
 
 function displayItems(items){
     const container = document.querySelector(".items");
@@ -22,4 +21,22 @@ function createHTMLString(item){
                 <img src="${item.image}" class="item_thumbnail" alt="${item.type}">
                 <span class="item_description">${item.gender}, ${item.size}</span>
             </li>`
+}
+
+function setEventListener(items){
+    const buttons = document.querySelector(".buttons");
+    const logo = document.querySelector(".logo");
+
+    logo.addEventListener("click", () => displayItems(items));
+    buttons.addEventListener("click", event => onButtonClick(event, items));
+}
+
+function onButtonClick(event, items){
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null) return;
+
+    displayItems(items.filter(item => item[key] === value));
 }
