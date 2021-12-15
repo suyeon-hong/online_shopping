8fbd38b4814d41747811d064b55791cf2f303a28
bellahong95 <bellahong95@gmail.com>
2021-12-15 19:51:18 +0900

    item invisible 처리하는 방식

--- js/main.js
+++ js/main.js
@@ -1,42 +1,56 @@
 
 loadItems()
 .then(items => {
-    displayItems(items);
-    setEventListener(items);
-})
-
-function loadItems(){
-    return fetch('../data/data.json')
-    .then(response => response.json())
-    .then(json => json.items)
-}
-
-function displayItems(items){
-    const container = document.querySelector(".items");
-    container.innerHTML = items.map(item => createHTMLString(item)).join('');
-}
-
-function createHTMLString(item){
-    return `<li>
-                <img src="${item.image}" class="item_thumbnail" alt="${item.type}">
-                <span class="item_description">${item.gender}, ${item.size}</span>
-            </li>`
-}
-
-function setEventListener(items){
-    const buttons = document.querySelector(".buttons");
+    const elements = items.map(createElement);
+    const container = document.querySelector('.items');
+    container.append(...elements);
+    const buttons = document.querySelector('.buttons');
+    buttons.addEventListener('click', event => onButtonClick(event, elements));
     const logo = document.querySelector(".logo");
+    logo.addEventListener("click", ()=>{
+        elements.forEach(el => el.classList.remove("invisible"))
+    });
+});
 
-    logo.addEventListener("click", () => displayItems(items));
-    buttons.addEventListener("click", event => onButtonClick(event, items));
+function loadItems() {
+    return fetch('data/data.json')
+        .then(response => response.json())
+        .then(json => json.items);
+    }
+
+function createElement(item) {
+    const img = document.createElement('img');
+    img.setAttribute('class', 'item_thumbnail');
+    img.setAttribute('src', item.image);
+
+    const span = document.createElement('span');
+    span.setAttribute('class', 'item_description');
+    span.innerText = `${item.gender}, ${item.size} size`;
+    const li = document.createElement('li');
+    li.setAttribute('class', 'item');
+    li.setAttribute('data-type', item.type);
+    li.setAttribute('data-color', item.color);
+    li.append(img);
+    li.append(span);
+    return li;
 }
 
-function onButtonClick(event, items){
-    const dataset = event.target.dataset;
-    const key = dataset.key;
-    const value = dataset.value;
+function onButtonClick(event, items) {
+    const target = event.target;
+    const key = target.dataset.key;
+    const value = target.dataset.value;
+    if (key == null || value == null) {
+        return;
+    }
+    updateItems(items, key, value);
+}
 
-    if(key == null || value == null) return;
-
-    displayItems(items.filter(item => item[key] === value));
+function updateItems(items, key, value) {
+    items.forEach(item => {
+        if (item.dataset[key] === value) {
+        item.classList.remove('invisible');
+        } else {
+        item.classList.add('invisible');
+        }
+    });
 }
